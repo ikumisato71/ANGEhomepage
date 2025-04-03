6820241bbdb2446d7d61ba5d99511936c6204ea6
ikumisato <kinoko7971@gmail.com>
2025-04-03 12:40:44 +0900

    画像やCSSを追加

--- js/main.js
+++ js/main.js
@@ -55,3 +55,51 @@ const mySwiper = new Swiper(".mv02 .swiper", {
     },
   },
 });
+
+const slideLength = document.querySelectorAll(
+  ".gallery03 .swiper-a .swiper-slide"
+).length;
+
+const params = {
+  slidesPerView: "auto",
+  loop: true,
+  loopedSlides: slideLength,
+  speed: 6000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  freeMode: {
+    enabled: true,
+    momentum: false,
+  },
+  grabCursor: true,
+};
+
+const initSwiper = () => {
+  const mySwiper_a = new Swiper(".gallery03 .swiper-a", {
+    ...params,
+    on: {
+      touchEnd: (swiper) => {
+        swiper.slideTo(swiper.activeIndex + 1);
+      },
+    },
+  });
+
+  const mySwiper_b = new Swiper(".gallery03 .swiper-b", {
+    ...params,
+    autoplay: {
+      ...params.autoplay,
+      reverseDirection: true,
+    },
+    on: {
+      touchEnd: (swiper) => {
+        swiper.slideTo(swiper.activeIndex - 1);
+      },
+    },
+  });
+};
+
+window.addEventListener("load", function () {
+  initSwiper();
+});
